2fa134c9af82e2aed410009c618636a6db994565
giangnt6988 <giangnt6988@gmail.com>
2016-04-09 09:45:41 +0700

    intergrated gulp-html-tag-include

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,15 @@ var less = require('gulp-less-sourcemap');
 var cssmin = require('gulp-cssmin');
 var gutil = require('gulp-util');
 var rename = require('gulp-rename');
+var includeHTML = require('gulp-html-tag-include');
 var browserSync = require('browser-sync').create();
 
 // /////////////////////////////////////////////
-// Build CSS And Minify CSS Task
+// Build CSS Task
 // /////////////////////////////////////////////
 gulp.task('build-css', function () {
     // Buil CSS
-    gulp.src(['./assets/less/**/*.less', '!./assets/less/**/_*.less'])
+    return gulp.src(['./assets/less/**/*.less', '!./assets/less/**/_*.less'])
         .pipe(plumber())
         .pipe(less({
             sourceMap: {
@@ -26,8 +27,14 @@ gulp.task('build-css', function () {
         .pipe(browserSync.stream())
         .pipe(gulp.dest('./assets/css')).on('error', gutil.log)
 
+});
+
+// /////////////////////////////////////////////
+// Minify CSS Task
+// /////////////////////////////////////////////
+gulp.task('minify-css', function() {
     // Minify CSS
-    gulp.src(['./assets/css/**/*.css', '!assets/css/**/*.min.css'])
+    return gulp.src(['./assets/css/**/*.css', '!assets/css/**/*.min.css'])
         .pipe(plumber())
         .pipe(cssmin({
             keepSpecialComments: false,
@@ -38,6 +45,17 @@ gulp.task('build-css', function () {
         }))
         .pipe(gulp.dest('./assets/css')).on('error', gutil.log);
 });
+
+// /////////////////////////////////////////////
+// BUILD HTML TASK
+// /////////////////////////////////////////////
+gulp.task('build-html', function() {
+    return gulp.src('./source/*.html')
+        .pipe(includeHTML())
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
+});
+
 
 // /////////////////////////////////////////////
 // Browser Sync Task
@@ -53,12 +71,14 @@ gulp.task('browser-sync', function() {
 // /////////////////////////////////////////////
 // WATCH TASK
 // /////////////////////////////////////////////
-gulp.task('watch', ['browser-sync'], function () {
+gulp.task('watch', ['build-html','browser-sync'], function () {
+    gulp.watch('./source/**/*.html', ['build-html']);
     gulp.watch('./assets/less/**/*.less', ['build-css']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch('./assets/less/**/*.less', ['build-css']);
+    //gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 // /////////////////////////////////////////////
 // DEfAULT TASK
 // /////////////////////////////////////////////
-gulp.task('default', ['watch']);
+gulp.task('default', ['build-html', 'build-css', 'minify-css', 'watch']);
